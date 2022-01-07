9c0d7259ce78665fbbd890c0298e608c85ee4e56
GarikPythonist <garik_margaryan2007@mail.ru>
2022-01-07 22:36:08 +0400

    hi

--- js/anim.js
+++ js/anim.js
@@ -1,16 +1,9 @@
- var lastScrollTop = 0;
-$(window).scroll(function(event){
-   var $nav = $('#main_ul');
-   var $elem = $('.list_elem')
-   var st = $(this).scrollTop();
-   if (st > lastScrollTop){
-    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
-    $elem.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
-       } else {
-    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
-    $elem.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
-    $nav.toggleClass('noscrolled', $(this).scrollTop() > $nav.height());
-    $elem.toggleClass('noscrolled', $(this).scrollTop() > $nav.height());
-   }
-});
+function openNav() {
+  $('#mySidenav').css('width', '247px');
+  $('#burger').hide();
+}
 
+function closeNav() {
+   $('#mySidenav').css('width', '0');
+   $('#burger').show();
+}
